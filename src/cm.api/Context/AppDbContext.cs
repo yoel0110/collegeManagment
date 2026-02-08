e1258849999f4c0493ddf07d1398abf570f544ba
Yoel 4Devs <Yoellebron35@gmail.com>
2026-02-08 18:11:45 -0400

    Add: OnModelCreating for control model creation flow

--- src/cm.api/Context/AppDbContext.cs
+++ src/cm.api/Context/AppDbContext.cs
@@ -11,11 +11,18 @@ namespace cm.api.Context
         }
 
         public DbSet<AcademicRecord> AcademicRecords { get; set; }
-        public DbSet<CatalogCourse> CatalogCourses { get; set; }
+        public DbSet<CatalogCourse> CatalogCourses { get; set; }  
         public DbSet<Department> Departments { get; set; } 
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Faculty> Faculties { get; set; }
         public DbSet<Professor> Professors { get; set; }
         public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AcademicRecord>()
+                .Property(p => p.RecordID)
+                .ValueGeneratedOnAdd();
+        }
     }
 }
